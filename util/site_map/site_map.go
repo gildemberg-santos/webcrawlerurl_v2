e901dd8b79a0c73f77ffbcb3d4a414f734c67776
gildemberg-santos <gildemberg.santos@gmail.com>
2024-06-29 12:09:10 -0300

    refactor: Criar busca de sitemap

---
+++ util/site_map/site_map.go
@@ -0,0 +1,65 @@
+package site_map
+
+import (
+	"encoding/xml"
+	"io"
+	"net/http"
+	"time"
+
+	useragent "github.com/gildemberg-santos/webcrawlerurl_v2/util/user_agent"
+)
+
+type SiteMap struct {
+	UrlLocation string `xml:"-"`
+	Url         *[]struct {
+		Loc      string    `xml:"loc"`
+		Lastmod  time.Time `xml:"lastmod"`
+		Priority float32   `xml:"priority"`
+	}
+}
+
+func NewSiteMap(url string) *SiteMap {
+	return &SiteMap{
+		UrlLocation: url,
+	}
+}
+
+func (s *SiteMap) Call() error {
+	if err := s.load(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *SiteMap) load() error {
+	client := &http.Client{Timeout: 10 * time.Second}
+	req, err := http.NewRequest("GET", s.UrlLocation, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("User-Agent", useragent.NewUserAgentRandom().Call().UserAgent)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	err = xml.Unmarshal(body, &s.Url)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
