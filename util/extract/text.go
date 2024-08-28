a9fae915508ea6cebab02659b2c04f6dc820fae7
gildemberg-santos <gildemberg.santos@gmail.com>
2024-08-28 12:14:00 -0300

    refactor: Normalize HTML in the text extraction process

--- util/extract/text.go
+++ util/extract/text.go
@@ -1,6 +1,7 @@
 package extract
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -18,9 +19,18 @@ func NewText(source *goquery.Document) Text {
 }
 
 func (t *Text) Call() *Text {
+	t.normalizeHTML()
 	t.Text = t.Sources.Find("body").Text()
 	t.normalize()
 	return t
+}
+
+func (t *Text) normalizeHTML() {
+	html, _ := t.Sources.Html()
+	re := regexp.MustCompile(`</(div|p|h1|h2|h3|h4|h5|h6|li|ul|ol|table|tr|td|th|blockquote|pre|code|section|article|aside|footer|header|nav|figure|figcaption|main|mark|nav|section|summary)>`)
+	html = re.ReplaceAllString(html, "</$1> ")
+
+	t.Sources, _ = goquery.NewDocumentFromReader(strings.NewReader(html))
 }
 
 func (t *Text) normalize() {
