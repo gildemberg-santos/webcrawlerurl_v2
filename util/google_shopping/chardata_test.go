179fc6cd59990dda295dc38c8e5efd3fd6a87f79
gildemberg-santos <gildemberg.santos@gmail.com>
2024-11-13 11:59:46 -0300

    Refactor: Adicionar novos testes para a função NewChardata, cobrindo casos de strings vazias, espaços, quebras de linha e conteúdo misto

--- util/google_shopping/chardata_test.go
+++ util/google_shopping/chardata_test.go
@@ -7,8 +7,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestChardata_NewChardata(t *testing.T) {
+	charData := googleshopping.NewChardata("1")
+
+	assert.New(t).Equal("1", charData.Value)
+}
+
 func TestChardata_Normalize(t *testing.T) {
 	charData := googleshopping.NewChardata("\n 1 \n")
 
 	assert.Equal(t, "1", charData.Value)
+}
+
+func TestChardata_EmptyString(t *testing.T) {
+	charData := googleshopping.NewChardata("")
+
+	assert.Equal(t, "", charData.Value)
+}
+
+func TestChardata_OnlySpaces(t *testing.T) {
+	charData := googleshopping.NewChardata("   ")
+
+	assert.Equal(t, "", charData.Value)
+}
+
+func TestChardata_OnlyNewlines(t *testing.T) {
+	charData := googleshopping.NewChardata("\n\n\n")
+
+	assert.Equal(t, "", charData.Value)
+}
+
+func TestChardata_SpacesAndNewlines(t *testing.T) {
+	charData := googleshopping.NewChardata(" \n  \n ")
+
+	assert.Equal(t, "", charData.Value)
+}
+
+func TestChardata_MixedContent(t *testing.T) {
+	charData := googleshopping.NewChardata(" \n 1 \n 2 \n ")
+
+	assert.Equal(t, "1 2", charData.Value)
 }
