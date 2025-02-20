26ea8d12627a45baa177446eb07bfa9da57b2e47
gildemberg-santos <gildemberg.santos@gmail.com>
2025-02-20 08:47:38 -0300

    feat: adicionar servidor gRPC com método SayHello

---
+++ cmd/grpc-serve/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/gildemberg-santos/webcrawlerurl_v2/grpc"
+	grpc "google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
