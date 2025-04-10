07d6e5405de3c191bd62687e70951a7bb808fc20
gildemeber-santos <gildemeber.santos@gmail.com>
2025-04-10 16:26:12 -0300

    refactor: update sitemap handling and error logging

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -13,19 +13,25 @@ func main() {
 		UrlPattern        string
 		UrlSiteMapPattern string
 	}{
-		Url:               "https://www.usaflex.com.br/sitemap.xml",
-		UrlPattern:        "https://www.usaflex.com.br/ac**/p",
+		Url:               "https://www.remyntima.com.br/",
+		UrlPattern:        "",
 		UrlSiteMapPattern: "",
 	}
 
 	log.Println("Starting")
 	log.Println("Loading sitemap -> ", sitemap.Url)
-	urls := pkg.NewEcommerceSitemap(sitemap.Url, sitemap.UrlPattern, sitemap.UrlSiteMapPattern).Call()
-	fileJson := file.NewFileJson("urls.json", urls)
-	fileJson.Save()
-	log.Println("Loading data")
-	data := pkg.NewEcommerce(urls.Urls, 1_000_000_000, true, true).Call()
-	fileJson = file.NewFileJson("data.json", data)
+	//urls := pkg.NewEcommerceSitemap(sitemap.Url, sitemap.UrlPattern, sitemap.UrlSiteMapPattern).Call()
+	//fileJson := file.NewFileJson("urls.json", urls)
+	//fileJson.Save()
+	//log.Println("Loading data")
+	//data := pkg.NewEcommerce(urls.Urls, 1_000_000_000, true, true).Call()
+	urls := pkg.NewMappingUrl(sitemap.Url, 1_000_000_000, true, nil)
+	data, err := urls.Call()
+	if err != nil {
+		log.Println("Error:", err)
+		return
+	}
+	fileJson := file.NewFileJson("data.json", data)
 	fileJson.Save()
 	log.Println("Completed")
 }
